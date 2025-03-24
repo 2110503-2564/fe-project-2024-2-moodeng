08a3682597271a94c50f809b1d3dad2379628098
Atipat Buranavatanachoke <101798829+atipata@users.noreply.github.com>
2025-03-24 16:14:30 +0700

    cache

--- moodeng-restaurant-reservation/src/libs/getRestaurants.tsx
+++ moodeng-restaurant-reservation/src/libs/getRestaurants.tsx
@@ -3,8 +3,11 @@ import { resolve } from "path";
 export default async function getRestaurants() {
     //await new Promise((resolve)=>setTimeout(resolve,5000) )
 
-    const response=await fetch("http://localhost:5000/api/v1/restaurants", {next:{tags:['restaurant']}});
-
+    const response = await fetch("http://localhost:5000/api/v1/restaurants", { 
+        cache: "no-store",
+        next: { tags: ["restaurant"] } 
+    });
+    
     if(!response.ok){
         throw new Error("failed to loaded the restaurants")
     }
